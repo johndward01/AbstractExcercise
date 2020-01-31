54a76594b2e04269c617b089f8054d60fbdd3ba9
johnd <johndward01@yahoo.com>
2020-01-31 10:36:15 -0600

    Add project files.

---
+++ AbstractExcercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AbstractExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var civic = new Car();
+            var hayabusa = new Motorcycle();
+            hayabusa.Drive();
+            civic.Drive();
+        }
+    }
+}
---
+++ AbstractExcercise/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractExcercise
+{
+    public abstract class Vehicle
+    {
+        public  int Wheels { get; set; }
+        public int Doors { get; set; }
+        public abstract void Drive();
+    }
+}
